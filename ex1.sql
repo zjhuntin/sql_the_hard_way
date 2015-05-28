9c588e4494dde9b2d748b7785d2ede1a59f3dcea
Zachary Huntington-Meath <zjhuntingtonmeath@gmail.com>
2015-05-28 09:52:19 -0400

    initial commit

---
+++ ex1.sql
@@ -0,0 +1,21 @@
+CREATE TABLE person (
+	id INTEGER PRIMARY KEY,
+	first_name TEXT,
+	last_name TEXT,
+	age INTEGER,
+	address TEXT,
+	zipcode INTEGER
+);
+
+CREATE TABLE pet (
+	id INTEGER PRIMARY KEY,
+	name TEXT,
+	age INTEGER,
+	breed TEXT,
+	dead TEXT
+);
+
+CREATE TABLE person_pet (
+	person_id INTEGER,
+	pet_id INTEGER
+);
